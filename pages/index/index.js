74c46a9514a0d024ef4aef74136b19785b7df75a
tangweikun <819573105@qq.com>
2019-10-19 16:02:19 +0800

    :construction: fresh every 5 seconds

--- pages/index/index.js
+++ pages/index/index.js
@@ -26,6 +26,22 @@ Page({
     });
   },
 
+  onShow: function() {
+    const that = this;
+    app.globalData.timeId = setInterval(() => {
+      wx.request({
+        url: 'http://piaofang.maoyan.com/second-box',
+        success(res) {
+          that.setData({ movies: res.data.data.list });
+        },
+      });
+    }, 5000);
+  },
+
+  onHide: function() {
+    clearInterval(app.globalData.timeId);
+  },
+
   onPullDownRefresh: function() {
     const that = this;
     wx.request({
