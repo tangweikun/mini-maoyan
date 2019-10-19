9459845af1d6e0a108a86dbf36e1d85c25d1a954
tangweikun <819573105@qq.com>
2019-10-19 15:49:27 +0800

    :construction: refresh on pull down

--- pages/index/index.js
+++ pages/index/index.js
@@ -25,4 +25,14 @@ Page({
       },
     });
   },
+
+  onPullDownRefresh: function() {
+    const that = this;
+    wx.request({
+      url: 'http://piaofang.maoyan.com/second-box',
+      success(res) {
+        that.setData({ movies: res.data.data.list });
+      },
+    });
+  },
 });
